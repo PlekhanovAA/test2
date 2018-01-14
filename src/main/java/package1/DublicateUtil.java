151fc687a98d7df1a59ce40ea04398be47bc7b13
PlekhanovAA <repn@yandex.ru>
2018-01-15 00:19:11 +0400

    done8

--- src/main/java/package1/DublicateUtil.java
+++ src/main/java/package1/DublicateUtil.java
@@ -1,21 +1,13 @@
 package package1;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DublicateUtil {
 
     public static <T> Collection<T> findElementsWithoutDublicates(Collection<T> input) {
-        HashSet<T> result =  new HashSet<>();
         Map<T, Long> map2 = input.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
-
-        for (Map.Entry<T, Long> entry : map2.entrySet()) {
-            if (entry.getValue() == 1) {
-                result.add(entry.getKey());
-            }
-        }
-        return result;
+        return map2.entrySet().stream().filter(map -> map.getValue() == 1).map(Map.Entry::getKey).collect(Collectors.toSet());
     }
 }
